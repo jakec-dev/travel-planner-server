8b7dc73026f5dae4872c6174147fd2853d2a1f21
Jake <jake@jakec.dev>
2022-06-28 13:41:59 +1000

    Change folder structure for tests

---
+++ Controllers/itemsController.test.js
@@ -0,0 +1,12 @@
+const { assert } = require("chai");
+
+describe("Controllers/itemsController.js", function () {
+  describe("addItem()", function () {
+    it("should return the new item", function () {
+      assert.equal(true, true);
+    });
+    it("should return an error if the new item's ID already exists", function () {
+      assert.equal(false, false);
+    });
+  });
+});
