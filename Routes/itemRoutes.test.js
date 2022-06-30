14d840a675a191f03f4d5b96fba8ef6b7b92ee2c
Jake <jake@jakec.dev>
2022-06-30 15:33:06 +1000

    Create POST route test

--- Routes/itemRoutes.test.js
+++ Routes/itemRoutes.test.js
@@ -7,11 +7,13 @@ const { expect } = chai;
 
 describe("Routes/itemRoutes", function () {
   describe("GET /items", function () {
-    it("returns all items", function () {
+    it("should return all items", function () {
       chai
         .request(app)
         .get("/items")
-        .end((_err, resp) => {
+        .end((err, resp) => {
+          expect(resp).to.have.status(200);
+          expect(err).to.be.null;
           expect(resp.body.data).to.eql([
             { id: 1, name: "Backpack", brand: "Osprey" },
             { id: 2, name: "Shoes", brand: "Nike" },
@@ -21,12 +23,48 @@ describe("Routes/itemRoutes", function () {
     });
   });
 
+  describe("POST /items", function () {
+    it("should return the new item", function () {
+      const newItem = {
+        id: 5,
+        name: "test name",
+        brand: "test brand",
+      };
+      chai
+        .request(app)
+        .post("/items")
+        .send(newItem)
+        .end((err, resp) => {
+          expect(resp).to.have.status(200);
+          expect(err).to.be.null;
+          expect(resp.body.data).to.eql(newItem);
+        });
+    });
+    it("should return an error if no name is provided", function () {
+      const newItem = {
+        id: 5,
+        brand: "test brand",
+      };
+      chai
+        .request(app)
+        .post("/items")
+        .send(newItem)
+        .end((err, resp) => {
+          expect(resp).to.have.status(422);
+          expect(err).to.be.null;
+          expect(resp.body.status).to.eql("error");
+        });
+    });
+  });
+
   describe("GET /items/:id", function () {
-    it("returns item if ID exists", function () {
+    it("should returns item if ID exists", function () {
       chai
         .request(app)
         .get("/items/2")
-        .end((_err, resp) => {
+        .end((err, resp) => {
+          expect(resp).to.have.status(200);
+          expect(err).to.be.null;
           expect(resp.body.data).to.eql({
             id: 2,
             name: "Shoes",
