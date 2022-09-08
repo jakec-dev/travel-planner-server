c501d1c4b815f009182b9962dfe5397fefc87fb6
Jake Clayton <jake@jakec.dev>
2022-09-08 15:43:49 +1000

    Create resolve for single item fetch

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -2,6 +2,35 @@ const itemsService = require("../services/itemsService");
 
 const resolvers = {
   Query: {
+    item: async (_, { id }) => {
+      const itemId = parseInt(id, 10);
+      try {
+        if (Number.isNaN(itemId)) {
+          return {
+            status: "error",
+            message: "Item ID is not a number",
+          };
+        }
+        const result = await itemsService.getItemWithId(itemId);
+        if (!result) {
+          return {
+            status: "error",
+            message: `No item with ID ${itemId} exists`,
+          };
+        }
+        return {
+          status: "success",
+          message: "Item fetched successfully",
+          data: result,
+        };
+      } catch (err) {
+        return {
+          status: "error",
+          message: err.message,
+        };
+      }
+    },
+
     items: async () => {
       try {
         const result = await itemsService.getItems();
