68b51ab6f9e3429262cd788d2a64c9f39c52cc94
Jake Clayton <jake@jakec.dev>
2022-09-09 15:03:42 +1000

    Add plugin to allow database connection close

--- src/server.js
+++ src/server.js
@@ -1,38 +1,42 @@
 const express = require("express");
 const cors = require("cors");
 const { ApolloServer } = require("apollo-server-express");
+const { ApolloServerPluginDrainHttpServer } = require("apollo-server-core");
+const http = require("http");
 const typeDefs = require("./graphql/schema");
 const resolvers = require("./graphql/resolvers");
-// const db = require("./utils/db");
+const db = require("./utils/db");
 const itemsRoutes = require("./routes/itemsRoutes");
 const apiDocsRoutes = require("./routes/apiDocsRoutes");
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const PORT = process.env.PORT || 5000;
 
-const main = async () => {
-  const app = express();
-  const port = process.env.PORT || 5000;
-  app.set("port", port);
-  app.use(cors());
-  app.use(express.json());
-  app.use("/items", itemsRoutes);
-  app.use("/api-docs", apiDocsRoutes);
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/items", itemsRoutes);
+app.use("/api-docs", apiDocsRoutes);
 
+const httpServer = http.createServer(app);
+
+const startApolloServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
   await server.start();
   server.applyMiddleware({ app });
 
-  app.listen(app.get("port"), () => {
-    // eslint-disable-next-line no-console
-    console.log(
-      `Server ready at http://localhost:${port}${server.graphqlPath}`
-    );
+  await httpServer.listen({ port: PORT });
+  httpServer.on("close", () => {
+    db.disconnect();
   });
+
+  // eslint-disable-next-line no-console
+  console.log(`Server ready at http://localhost:${PORT}${server.graphqlPath}`);
 };
 
-main();
+startApolloServer();
 
-// server.on("close", () => {
-//   db.disconnect();
-// });
-
-module.exports = server;
+module.exports = httpServer;
